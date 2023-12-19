62f73b78c1f46115617b599916c9e176caab1455
Narendra Anbazhagan <131426493+narendraanbazhagann@users.noreply.github.com>
2023-12-19 07:51:49 +0530

    Create Math.h

---
+++ Math.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,pr;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    pr=pow(num,2);
+    printf("The power of %d is %d",num,pr);
+}
